92d1cd2d05eb039668a2061fd586c99a32fef9ee
olesia <infocooll.col@gmail.com>
2026-08-30 19:00:07 +0200

    added html, css,js

---
+++ src/script.js
@@ -0,0 +1,25 @@
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const secondHand = document.querySelector(".second-hand");
+
+function setClock() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondDegrees = seconds * 6;
+  const minuteDegrees = minutes * 6;
+  const hourDegrees = hours * 30 + minutes * 0.5;
+
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setClock();
+
+setInterval(setClock, 1000);
