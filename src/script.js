fd684f04a41980e872e7d41d6e871b459fe57975
olesia <infocooll.col@gmail.com>
2026-08-31 19:58:07 +0200

    ijected more weather data

--- src/script.js
+++ src/script.js
@@ -27,9 +27,39 @@ function updateWeather(response) {
   let temperatureElemment = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElemment = document.querySelector("#weather-app-city");
+  let descriptionElemment = document.querySelector("#description");
+  let humidityElemment = document.querySelector("#humidity");
+  let windElemment = document.querySelector("#wind");
+  let timeElemment = document.querySelector("#time");
+  let data = new Date(response.data.time * 1000);
+
+
+
   cityElemment.innerHTML = response.data.city;
   temperatureElemment.innerHTML = Math.round(temperature);
-  console.log(response.data.temperature.current);
+  descriptionElemment.innerHTML = response.data.condition.description;
+  humidityElemment.innerHTML = `${response.data.temperature.humidity}%`;
+  windElemment.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElemment.innerHTML = formatDate(data);
+}
+
+function formatDate(data) {
+  let minutes = data.getMinutes();
+  let houers = data.getHours();
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[data.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${houers}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = "7561c89f6et45b832o8e3c5fcade60b0";
